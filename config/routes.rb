7ce87707d15aac79bd428bd09aec61c370c5d526
Rosie Hoyem <rosiehoyem@gmail.com>
2017-01-22 19:22:42 -0600

    added theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'user#index'
+  
+  get 'projects/benson'
+
+  root to: 'pages#home'
+
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
